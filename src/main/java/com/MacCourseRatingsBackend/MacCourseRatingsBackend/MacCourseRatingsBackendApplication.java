5a3306f25ae6d6c56b52684eb5f8676076eb9358
patrickzyeh <patrickzyeh@gmail.com>
2025-11-13 18:33:54 -0500

    configurated docker postgres

---
+++ src/main/java/com/MacCourseRatingsBackend/MacCourseRatingsBackend/MacCourseRatingsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.MacCourseRatingsBackend.MacCourseRatingsBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacCourseRatingsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacCourseRatingsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MacCourseRatingsBackend/MacCourseRatingsBackend/MacCourseRatingsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MacCourseRatingsBackend.MacCourseRatingsBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacCourseRatingsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
